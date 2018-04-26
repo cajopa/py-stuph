4e8537ff3b78aa54b1882d974aee221ee380b5f5
Carl Johnson <carl@protectamerica.com>
2018-04-26 13:40:57 -0500

    (probably) finished State, implemented Peekable for use in pattern-matching potential next states

--- statemachine.py
+++ statemachine.py
@@ -1,37 +1,44 @@
+from collections import defaultdict
+
+
 class State:
     def __init__(self, parent):
         self.data = None
         self.parent = parent
     
-    @classmethod
-    def from_parameters(self, should_enter=lambda x: False, on_enter=lambda x: None):
-        pass #TODO: implement
+    @property
+    def name(self):
+        return self.__class__.__name__
     
-    def should_enter(self, value):
-        'is this State eligible for entry?'
-        
+    def _recognize(self, value):
+        return None #tri-value return: False means bad, True means good, None means dunno
+    
+    def _accept(self, value):
         pass
     
-    def on_enter(self, value):
-        'do stuff after entry (should only affect self.data)'
+    def accept(self, value):
+        '''
+        Try to take data. If it doesn't apply, raise Unrecognized.
+        '''
         
-        pass
-    
-    def try_entry(self, value):
-        if self.should_enter(value):
-            self.on_enter(value)
-            
-            return self #return self or self.data? basically comes down to: instantiate or reuse?
+        if self._recognize(value) is False: #tri-value, so test for identity
+            raise Unrecognized
         else:
-            return None
+            try:
+                self._accept(value)
+                
+                return self
+            except:
+                raise Unrecognized
 
 class StateMachine(State):
     def __init__(self, states):
         self.active_submachine = None
+        self.data = defaultdict(list)
         
-        self.states = {state.__class__.__name__: state(self) for state in states}
+        self.states = states
     
-    def eat(self, value):
+    def accept(self, values):
         '''
         if I have an active submachine,
          keep passing data to it until it returns #iterator-based? (next(feed) if consuming, pass if submachine)
@@ -43,4 +50,41 @@ class StateMachine(State):
           terminate
         '''
         
-        pass
+        if self.active_submachine:
+            #give the submachine the iterator so it can eat all it wants
+            self.data[self.active_submachine.name].append(self.active_submachine.accept(values))
+        else:
+            for value in values:
+                for state in self.states:
+                    try:
+                        self.data[state.__name__].append(state(self).accept(value))
+                    except Unrecognized:
+                        pass #intentional
+
+class Peekable:
+    def __init__(self, iterator):
+        self.iterator = iter(iterator) #iter() is idempotent
+        self.current = None
+    
+    def __iter__(self):
+        return self
+    
+    def __next__(self):
+        current, self.current = self.current, None
+        
+        if current:
+            return current
+        else:
+            return next(self.iterator)
+    
+    def peek(self):
+        if not self.current:
+            self.current = next(self.iterator)
+        
+        return self.current
+
+class BaseSignal(Exception):
+    pass
+
+class Unrecognized(BaseSignal):
+    pass
